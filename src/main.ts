9c3f46ebdcb964ddf61b291f74e8cb3d0365f6d0
Ruben David <ruben@jobs2live.com>
2024-04-26 13:27:20 -0400

    Preparando proyecto para curso de Angular de Fernando Herrera

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, appConfig)
+import { AppModule } from './app/app.module';
+import { enviroment } from './app/envoriments/enviroment.prod';
+
+if (enviroment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
