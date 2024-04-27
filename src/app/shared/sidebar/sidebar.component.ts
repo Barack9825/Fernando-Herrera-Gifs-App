233edc5adfb41841fb7d6f0b4f5db4dc766706a0
Ruben David <ruben@jobs2live.com>
2024-04-27 15:27:21 -0400

    Fernando Herrera Sec-7 017  Obtener Imagenes desde el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,13 +4,15 @@ import { GifsService } from '../../gifs/services/gifs.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-
 })
 export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
 
-  constructor(private gifsService: GifsService) { }
-  
   get historical() {
-    return this.gifsService.historial
+    return this.gifsService.historial;
+  }
+
+  buscar(record: string) {
+    this.gifsService.buscarGifs(record);
   }
 }
