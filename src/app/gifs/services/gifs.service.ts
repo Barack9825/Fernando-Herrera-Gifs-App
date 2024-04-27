18a96e99b0edc436139c7687a5bcd7ac99d90fac
Ruben David <ruben@jobs2live.com>
2024-04-27 15:53:08 -0400

    Fernando Herrera Sec-7 018 HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 
@@ -7,6 +7,7 @@ import { Gif, SearchGifsResponse } from '../interfaces/gifs.interfaces';
 })
 export class GifsService {
   private apikey: string = 'lnYJFStfYykY9u3ddOKnKH7PsKooC1tP';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   // TODO: Cambiar any por su tipo correspondoente
@@ -19,7 +20,7 @@ export class GifsService {
   constructor(private http: HttpClient) {
     this._historial = JSON.parse(localStorage.getItem('Historial')!) || [];
     this.resultados = JSON.parse(localStorage.getItem('Imagenes')!) || [];
-   
+
     /* if (localStorage.getItem('Historial')) {
       this._historial = JSON.parse(localStorage.getItem('Historial')!);
     } */
@@ -33,18 +34,20 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
 
       localStorage.setItem('Historial', JSON.stringify(this._historial));
-      
     }
 
+    const params = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('limit', '10')
+      .set('q', query);
+
+    console.log(params.toString());
+
     this.http
-      .get<SearchGifsResponse>(
-        `https://api.giphy.com/v1/gifs/search?api_key=lnYJFStfYykY9u3ddOKnKH7PsKooC1tP&q=${query}&limit=10&`
-      )
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params })
       .subscribe((resp) => {
-        
         this.resultados = resp.data;
-      
-        
+
         localStorage.setItem('Imagenes', JSON.stringify(this.resultados));
       });
   }
